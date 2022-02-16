398ed09c0325eeb16a4e367055583eec4b1d6bb6
liamsmith3194 <smith.liam1994@gmail.com>
2022-02-16 20:45:39 +0000

    / after allowed hosts removed

--- aceofsteaks/settings.py
+++ aceofsteaks/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ace-of-steaks.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['ace-of-steaks.herokuapp.com', 'localhost']
 
 
 # Application definition
