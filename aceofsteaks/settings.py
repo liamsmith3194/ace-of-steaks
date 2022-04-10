485798962d40fa30871111b3a0d4527068bbcea1
liamsmith3194 <smith.liam1994@gmail.com>
2022-04-10 13:02:19 +0000

    emailjs working on refresh but not submit of form

--- aceofsteaks/settings.py
+++ aceofsteaks/settings.py
@@ -182,7 +182,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'booking.aceofsteaks@gmail.com'
-EMAIL_HOST_PASSWORD = 'cobblers99'
+EMAIL_HOST_PASSWORD = 'ciproject4!'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
