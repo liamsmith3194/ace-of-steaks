5af4e2a264d88024d9e0320612871d7a521d569b
liamsmith3194 <smith.liam1994@gmail.com>
2022-04-10 14:01:54 +0000

    only character fields able to be sent on emailjs

--- static/scripts/SendEmail.js
+++ static/scripts/SendEmail.js
@@ -1,22 +1,33 @@
-console.log('hi')
+// console.log('hi')
 
-function sendMail(bookingForm) {
-    emailjs.send("service_hc7jbil", "template_kh3yzfz", {
-        "location": bookingForm.location.value,
-        "date": bookingForm.date_time.value,
-        "guests": bookingForm.guests.value,
-        "to_fname": bookingForm.fname.value,
-        "to_lname": bookingForm.lname.value,
-        "to_email": bookingForm.email.value,
+// function sendMail() {
+//     var username = "{{ user.id }}";
+//     document.getElementById("user_id").value = username;
 
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        }
-    );
-    return false;  // To block from loading a new page
-}
+//     var email = "{{ user.email }}";
+//     document.getElementById("user_email").value = email;
+
+//     var fname = "{{ user.first_name }}";
+//     document.getElementById("user_first_name").value = fname;
+
+//     var lname = "{{ user.last_name }}";
+//     document.getElementById("user_last_name").value = lname;
+
+//     var templateParams = {
+//         fname: fname,
+//         email: email,
+//         location: location,
+//         // date: date,
+//         guests: guests,
+
+//     }
+
+//     console.log('SUCCESS!', fname, lname, email);
+
+//     emailjs.send("service_hc7jbil", "template_kh3yzfz", templateParams)
+//     .then(function (response) {
+//         console.log('SUCCESS!', response.status, response.text, fname, lname, email);
+//     }, function (error) {
+//         console.log('FAILED...', error);
+//     });
+// }
